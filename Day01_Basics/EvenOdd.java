581e87298d2ee3214f2bc09dfe0bc6871710339d
Axion007 <151603573+Axion007@users.noreply.github.com>
2026-01-30 02:07:50 +0530

    Update EvenOdd.java

--- Day01_Basics/EvenOdd.java
+++ Day01_Basics/EvenOdd.java
@@ -1,1 +1,13 @@
-1-<img width="1920" height="1080" alt="even" src="https://github.com/user-attachments/assets/80126c0d-a849-4f83-90f2-1d1a402a932a" />
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
