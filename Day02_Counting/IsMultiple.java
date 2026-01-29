8f3dcef589a0699ab3e20435cb1d174ce61e8ada
Axion007 <151603573+Axion007@users.noreply.github.com>
2026-01-30 02:22:44 +0530

    Create IsMultiple.java

---
+++ Day02_Counting/IsMultiple.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class IsMultiple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        if (n % m == 0)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
