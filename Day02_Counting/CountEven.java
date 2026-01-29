02498bd1603428b2cd95b81b5b1c34d40ed65b46
Axion007 <151603573+Axion007@users.noreply.github.com>
2026-01-30 02:23:16 +0530

    Create CountEven.java

---
+++ Day02_Counting/CountEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CountEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int x = sc.nextInt();
+            if (x % 2 == 0)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
